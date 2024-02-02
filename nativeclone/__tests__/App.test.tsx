bae79bf9b30de15e9936d08e50ebf72da19cec2c
rkendel1 <148293244+rkendel1@users.noreply.github.com>
2024-02-02 12:53:56 -0500

    trying

---
+++ nativeclone/__tests__/App.test.tsx
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../src/App';
+import type { AppProps } from '../src/App'; // Import the corresponding type declarations
+
+// Note: import explicitly to use the types shipped with jest.
+import {it} from '@jest/globals';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
